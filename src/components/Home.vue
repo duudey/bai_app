250fb7a9996ebc3120d4cae207335d4fe77a5986
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2020-03-29 13:57:39 +0200

    cleaining

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="homepage">
+    <h1>Witamy w naszej aplikacji, będziemy tutaj pracować</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style>
+</style>
