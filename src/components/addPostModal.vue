1e29ab4888e1a2d1b114d394e3534a1fa658fdac
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2020-04-09 18:33:33 +0200

    test modal

---
+++ src/components/addPostModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="addPostModal">
+    <!-- Using modifiers -->
+    <b-button v-b-modal.postModal>Dodaj post</b-button>
+    <!-- The modal -->
+    <b-modal id="postModal">Hello From My Modal()</b-modal>
+    <b-button>{{title}}</b-button>
+  </div>
+</template>
+
+<script>
+import { BButton } from 'bootstrap-vue';
+
+export default {
+  name: "addPostModal",
+  components: {BButton},
+  methods: {
+    showModal() {
+      console.log("modal działa");
+    }
+  },
+   props:['title']
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
