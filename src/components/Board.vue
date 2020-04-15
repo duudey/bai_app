5b049e52576c3e7077bafd9e041aa5922bab3094
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2020-04-15 13:38:56 +0200

    board working

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,24 +3,27 @@
     <b-container class="bv-example-row">
       <b-row class="text-center">
         <!-- starczy wrzucić jsona w miejsce result i powinno banglać -->
-        <b-col v-for="(item, idx) in result" :key="idx" class="m-1">
+        <b-col cols="3" v-for="(item, idx) in result" :key="idx" class="m-1">
           <div v-bind:class="'box box-'+item.id" class="row align-items-center p-2">
             <div class="col">
               <div id="name">{{item.name}}</div>
               <div id="category">{{item.category}}</div>
-              <span>Potrzebuję:</span>
-              <!-- Jest jakiś problem z wyświtlaniem
-            1. Wyświetla się po zapisaniu normalnie i wszystko funkcjonuje
-            2. Przy reloadzie znika przycisk i input modalu pojawia się na karcie, nie wiem, czym jest to wszystko spowodowane
-            -> do zrobienia
-              -->
+              <div class="text-left">
+              <b>Potrzebuję:</b>
               <div v-for="(el, idx) in item.basket" :key="idx">{{el.product}}: {{el.amount}}</div>
+              </div>
               <div class="row pt-2">
                 <div class="col">
                   <b-button v-b-modal="'postModal-'+item.id" class="btn btn-sm">Click to see details</b-button>
-                  <b-modal v-bind:id="'postModal-'+item.id" v-bind:title="item.name + ' #' +item.id">
+                  <b-modal
+                    v-bind:id="'postModal-'+item.id"
+                    v-bind:title="item.name + ' #' +item.id"
+                  >
                     <p>Potrzebuje:</p>
-                    <div v-for="(el, idx) in item.basket" :key="idx">{{el.product}}: {{el.amount}}</div>
+                    
+                    <div v-for="(el, idx) in item.basket" 
+                    :key="idx" >{{el.product}}: {{el.amount}}</div>
+                    <div class="pt-2">{{item.adress}}</div>
                   </b-modal>
                 </div>
               </div>
@@ -33,17 +36,25 @@
 </template>
 
 <script>
-import { BRow, BContainer, BCol , BButton, VBModal, BModal} from "bootstrap-vue";
+import {
+  BRow,
+  BContainer,
+  BCol,
+  BButton,
+  VBModal,
+  BModal
+} from "bootstrap-vue";
 
 export default {
-  name: 'Board',
-   components: {
+  name: "Board",
+  components: {
     BRow,
     BContainer,
-    BCol, BButton, 
-    'b-modal': BModal,
+    BCol,
+    BButton,
+    "b-modal": BModal
   },
-  directives: {'b-modal': VBModal},
+  directives: { "b-modal": VBModal },
   data() {
     return {
       result: [
@@ -55,7 +66,7 @@ export default {
             { product: "chleb", amount: 2, id: "ds321343" },
             { product: "ser kozi", amount: 43, id: "ds940390934" }
           ],
-          adress:'Ursyniowska 23, Rybnik',
+          adress: "Ursyniowska 23, Rybnik",
           id: 2
         },
         {
@@ -65,13 +76,12 @@ export default {
             { product: "komputer", amount: 1, id: "ds2133123" },
             { product: "myszka", amount: 3, id: "ds321343" }
           ],
-          adress:'Mariańska 4A, Poznań',
+          adress: "Mariańska 4A, Poznań",
           id: 23
-        }
-      ]
+        },
+      ],
     };
-  },
- 
+  }
 };
 </script>
 
